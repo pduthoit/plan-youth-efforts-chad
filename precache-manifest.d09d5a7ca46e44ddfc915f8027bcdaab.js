e33d3d51a56918dfee5f9c4ad4067e5d89a25aeb
Pierre-Louis <pl_duthoit@cartong.org>
2021-04-19 14:23:35 +0200

    [EDIT] Add PWA and basemap selector

---
+++ precache-manifest.d09d5a7ca46e44ddfc915f8027bcdaab.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a4d85a76dc77a19eb34",
+    "url": "css/app.8c988d68.css"
+  },
+  {
+    "revision": "a4b621c968d12217d4c1",
+    "url": "css/chunk-vendors.8e069941.css"
+  },
+  {
+    "revision": "294f2e27fd0602c32be5a6d86f3f0b6e",
+    "url": "fonts/Veneer_Regular.294f2e27.ttf"
+  },
+  {
+    "revision": "c4c64809048209fad42cad644a536363",
+    "url": "img/basemap-plan.c4c64809.png"
+  },
+  {
+    "revision": "8fb8ecc6ca990fa85b77a0665b403df4",
+    "url": "img/basemap-satellite.8fb8ecc6.png"
+  },
+  {
+    "revision": "0565f377f1ec289c6b8a2550270c55f1",
+    "url": "img/child-protection.0565f377.svg"
+  },
+  {
+    "revision": "b955b5f6f8fe779006565ba4f7f133a5",
+    "url": "img/community-building.b955b5f6.svg"
+  },
+  {
+    "revision": "4801c950f67209c63cb76ddbd82b2de0",
+    "url": "img/data.4801c950.svg"
+  },
+  {
+    "revision": "c28461fea9f386d2ffeed630e762e0d3",
+    "url": "img/education.c28461fe.svg"
+  },
+  {
+    "revision": "7b4534d284b8ca4889f4c4d8b56d1f9c",
+    "url": "img/eu_flag.7b4534d2.svg"
+  },
+  {
+    "revision": "87c8822ec49d59f15af6bcee42711789",
+    "url": "img/health.87c8822e.svg"
+  },
+  {
+    "revision": "c296e1a0347644896450277b44dfcba3",
+    "url": "img/layer.c296e1a0.svg"
+  },
+  {
+    "revision": "5fdbc9aae785f01b08ed54005b6dc933",
+    "url": "img/plan_logo.5fdbc9aa.svg"
+  },
+  {
+    "revision": "debaaa03406bd9fd3f4c79ccec8db709",
+    "url": "img/school.debaaa03.svg"
+  },
+  {
+    "revision": "d8099f50aa91a4e09f8fc20687a5b251",
+    "url": "img/sexual-violence.d8099f50.svg"
+  },
+  {
+    "revision": "d9be3e1bab2a0488dd98c5329238e06d",
+    "url": "img/training.d9be3e1b.svg"
+  },
+  {
+    "revision": "9dd81aa007ec055f0af0ea51629dc455",
+    "url": "img/youth-organizations.9dd81aa0.svg"
+  },
+  {
+    "revision": "8d1b05e7b8e67705d81f40923fc6739c",
+    "url": "index.html"
+  },
+  {
+    "revision": "5a4d85a76dc77a19eb34",
+    "url": "js/app.e842914b.js"
+  },
+  {
+    "revision": "a4b621c968d12217d4c1",
+    "url": "js/chunk-vendors.2006c913.js"
+  },
+  {
+    "revision": "69df80d76ad9dc57a63bfc9b53e710aa",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,33 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "precache-manifest.d09d5a7ca46e44ddfc915f8027bcdaab.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "less"});
+
+workbox.core.skipWaiting();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute(/^(https:\/\/cors-anywhere\.herokuapp\.com\/)?https:\/\/kobo\.humanitarianresponse\.info/, new workbox.strategies.NetworkFirst({ "cacheName":"api-cache","networkTimeoutSeconds":20, plugins: [new workbox.cacheableResponse.Plugin({ statuses: [ 0, 200 ] })] }), 'GET');
+workbox.routing.registerRoute(/https:\/\/api\.mapbox\.com/, new workbox.strategies.CacheFirst({ "cacheName":"mapbox-cache", plugins: [new workbox.cacheableResponse.Plugin({ statuses: [ 0, 200 ] }), new workbox.expiration.Plugin({ maxEntries: 120, purgeOnQuotaError: false })] }), 'GET');
